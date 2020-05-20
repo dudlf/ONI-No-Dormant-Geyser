a6076b5b75d3e1c2395089a6d459d9948290b263
dudlf <dudlf016@gmail.com>
2020-05-20 17:13:27 +0900

    초기 커밋.

---
+++ MainPatch.cs
@@ -0,0 +1,51 @@
+﻿using Harmony;
+using PeterHan.PLib.Options;
+using System;
+using System.Linq;
+
+namespace NoDormantGeyser
+{
+    public static class MainPatch
+    {
+        public static bool disable = false;
+
+        [HarmonyPatch(typeof(GeyserConfigurator.GeyserInstanceConfiguration))]
+        [HarmonyPatch("GetYearPercent")]
+        public static class YearPercentPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ref object __instance, ref float __result)
+            {
+                var geyserType = ((GeyserConfigurator.GeyserInstanceConfiguration)__instance).geyserType;
+                if (!disable && !Config.IsExcluded(geyserType))
+                    __result = 1f;
+            }
+        }
+
+        [HarmonyPatch(typeof(GeyserConfigurator.GeyserInstanceConfiguration))]
+        [HarmonyPatch("GetMassPerCycle")]
+        public static class MassPerCyclePatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ref object __instance, ref float ___scaledYearPercent, ref float ___scaledRate, ref float __result)
+            {
+                var config = (GeyserConfigurator.GeyserInstanceConfiguration)__instance;
+
+                if (!disable && !Config.IsExcluded(config) && (!GeyserCrackingPatch.disable.ContainsKey(config) || !GeyserCrackingPatch.disable[config]))
+                {
+                    __result = ___scaledRate * ___scaledYearPercent;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Game), "OnPrefabInit")]
+        public class Init
+        {
+            [HarmonyPrefix]
+            public static void Prefix()
+            {
+                disable = false;
+            }
+        }
+    }
+}
---
+++ MapSharingPatch.cs
@@ -0,0 +1,37 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace NoDormantGeyser
+{
+    [HarmonyPatch]
+    [HarmonyAfter("1825374565.steam")]
+    public static class MapSharingPatch
+    {
+        [HarmonyTargetMethods]
+        public static IEnumerable<MethodBase> TargetMethod(HarmonyInstance harmony)
+        {
+            var Utils = AccessTools.TypeByName("ToolsNotIncluded.ModCommon.CommonPatches+GeyserOnSpawn");
+
+            if (Utils == null)
+                yield break;
+
+            var postfix = AccessTools.Method(Utils, "Postfix", new Type[] { typeof(Geyser) });
+
+            yield return postfix;
+        }
+
+        [HarmonyPrefix]
+        public static void Prefix()
+        {
+            MainPatch.disable = true;
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix()
+        { 
+            MainPatch.disable = false;
+        }
+    }
+}
